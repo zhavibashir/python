b4b1e0a124b1131d2e260f57e97322435f4a3348
shafie bashir ali <shaficib00@gmail.com>
2022-09-22 09:09:10 +0300

    i modified my views.py :

--- quiz/quizapp/views.py
+++ quiz/quizapp/views.py
@@ -15,7 +15,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username= form.cleaned_data.get('username')
-            messages.success(request, f'account created for {username}')
+            messages.success(request, f'account created for this {username}')
             return redirect('/')
     else :
         form = UserRegisterForm()
